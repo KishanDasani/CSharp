aaffb5d9a0dace30fa89148c8866219142467869
KishanDasani <kishankdasani@gmail.com>
2019-12-26 09:28:51 +0530

    Commit after adding 3 patterns

--- Practical1/Program.cs
+++ Practical1/Program.cs
@@ -10,6 +10,54 @@ namespace Practical1
     {
         static void Main(string[] args)
         {
+            pattern1();
+            pattern2();
+            pattern3();
+        }
+
+        static void pattern1()
+        {
+            int i, j;
+            Console.WriteLine("Pattern1");
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine();
+        }
+
+        static void pattern2()
+        {
+            int i, j;
+            Console.WriteLine("Pattern2");
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j <=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine();
+        }
+
+        static void pattern3()
+        {
+            int i, j;
+            Console.WriteLine("Pattern3");
+            for (i = 5; i > 0; i--)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.Read();
         }
     }
 }
