4a7660bdbe4b90d213a13a37c841282acc6d2511
KishanDasani <kishankdasani@gmail.com>
2020-01-02 09:12:51 +0530

    Commiting after adding practical 2 for validating username and country with regular expressions

---
+++ Practical2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, country;
+            int flag = 0;
+
+            Console.WriteLine("Enter your good name");
+            username = Console.ReadLine();
+
+            Console.WriteLine("\nWhere are you from?");
+            country = Console.ReadLine();
+
+            if (Regex.IsMatch(username , @"^[a-zA-Z]+$") && Regex.IsMatch(country , @"^[a-zA-z]+$" ))
+                flag = 0;
+            else
+                flag = 1;
+
+            if (flag == 0)
+                Console.WriteLine("\nWelcome " + username + " from country " + country);
+            else
+                Console.WriteLine("\nPlease enter valid details.. your name and country inputs must contain only characters");
+
+            Console.ReadKey();
+        }
+    }
+}
