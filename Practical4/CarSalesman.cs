d07c0113d3434ccca51729fbc98186515c5cc9b1
KishanDasani <50163538+KishanDasani@users.noreply.github.com>
2020-01-23 08:45:36 +0530

    Initial Commit for practical for inheritance

---
+++ Practical4/CarSalesman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical4
+{
+    class CarSalesman : ISalesman
+    {
+        private String firstName;
+        private String lastName;
+
+        public CarSalesman(String firstName, String lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void sell()
+        {
+            Console.WriteLine("Hello! My name is " + firstName + " " + lastName + ". I recommend you to buy this car.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practical4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RetailSalesman RS = new RetailSalesman("Kishan", "Dasani");
+
+            RS.sell();
+
+            CarSalesman CS = new CarSalesman("Jay", "Dasani");
+
+            CS.sell();
+
+        }
+    }
+}
